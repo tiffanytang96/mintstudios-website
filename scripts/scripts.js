bfce6df7472f88fdc07fb07ad7a911f93acc5bbd
tiffanywgtang <tiffanywgtang@hotmail.com>
2020-03-07 16:13:31 -0800

    ig carousel in progress

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -127,5 +127,46 @@ $(document).ready(function(){
           }
         ]
       });
+
+      // Instagram section
+      $('.instagram-images').slick({
+        infinite: false,
+        slidesToShow: 6,
+        arrows: false,
+        autoplay: false,
+        dots: false,
+  
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 1,
+              infinite: false,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              autoplay:true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false,
+              autoplay:true
+            }
+          }
+        ]
+      });
   });
                   
